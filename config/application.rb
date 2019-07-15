f03df10d1a5d639e7411e3da3261b644c7dc5c75
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-07-15 19:12:48 +0900

    change:マイページの画像設定がない場合の表示変更

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,6 @@ module Instagramclone
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-
-    # I18n.config.available_locales = :ja
-    # I18n.default_locale = :ja
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
     config.generators do |g|

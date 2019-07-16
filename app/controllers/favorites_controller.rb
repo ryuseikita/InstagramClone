3ee5e0a97bfe60addad6201296c8cc39166419fa
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-07-17 00:32:43 +0900

    heroku用

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,7 @@ class FavoritesController < ApplicationController
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り登録しました"
   end
 
+ 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to pictures_url, notice: "#{favorite.picture.user.name}さんのブログをお気に入り解除しました"

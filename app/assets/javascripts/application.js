b716223a25d5f835a280259e19c0bf0dde979f3e
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-07-17 22:01:50 +0900

    heroku用

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
+//= require jquery/dist/jquery
 //= require_tree .

eb499cdb08307e7b74ea65089606c30a63262647
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-07-18 22:19:36 +0900

    fix:投稿確認メールの宛先が投稿者のアドレスに飛ぶよう変更

--- app/mailers/confirm_mailer.rb
+++ app/mailers/confirm_mailer.rb
@@ -2,6 +2,6 @@ class ConfirmMailer < ApplicationMailer
   def confirm_mail(picture)
     @picture = picture
 
-    mail to: "ryslsuzuki@gmail.com", subject: "投稿確認メール"
+    mail to: @picture.user.email, subject: "投稿確認メール"
   end
 end

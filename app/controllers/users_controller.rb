e70f5e3b56f70b7c14850995d59d1166c2b75705
鈴木竜生 <suzukiryusei@RYS-Macbook.local>
2019-07-19 23:57:10 +0900

    update:お気に入り投稿一覧・自分の投稿一覧の並びを降順に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to controller: 'profiles', action: 'create'      
+      redirect_to controller: 'profiles', action: 'create'
     else
       render 'new'
     end

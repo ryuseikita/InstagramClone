7d2cd4898994ee2a27c1d75a4ff88d3e3eee84dc
鈴木竜生 <suzukiryusei@RYS-MacBook.local>
2019-12-01 18:47:11 +0900

    fix:s3設定修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -18,10 +18,10 @@ CarrierWave.configure do |config|
   
     case Rails.env
       when 'production'
-        config.fog_directory = '本番環境用のバケット名'
+        config.fog_directory = 'instagramclone-achieve-production'
         config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/instagramclone-achieve-production'
       when 'development'
-        config.fog_directory = '開発環境用のバケット名'
+        config.fog_directory = 'instagramclone-achieve-development'
         config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/instagramclone-achieve-development'
     end
   end
